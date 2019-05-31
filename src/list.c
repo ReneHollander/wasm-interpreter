1b1792c5c40a00196fd44d4da6245a5c6d10bf2e
Markus Hinterleitner <m.hinterleitner93@gmail.com>
2019-05-31 07:55:00 +0200

    Adds initial list implementation.

---
+++ src/list.c
@@ -0,0 +1,59 @@
+#include "list.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "list.h"
+
+void insert_first(node_t **head_ref, void *new_data, uint32_t data_size) {
+    node_t *new_node = (node_t *) malloc(sizeof(node_t));
+
+    new_node->data = malloc(data_size);
+    new_node->next = (*head_ref);
+
+    int i;
+    for (i = 0; i < data_size; i++)
+        *(byte *) (new_node->data + i) = *(byte *) (new_data + i);
+
+    (*head_ref) = new_node;
+}
+
+void delete_first(node_t **head_ref) {
+    if (*head_ref == NULL) {
+        fprintf(stderr, "tried to delete element from empty list\n");
+        exit(EXIT_FAILURE);
+    }
+
+    node_t *tmp = *head_ref;
+    *head_ref = (*head_ref)->next;
+    free(tmp->data);
+    free(tmp);
+}
+
+void *get_at(node_t **head_ref, uint32_t idx) {
+    node_t *cur = *head_ref;
+
+    for (int i = 0; cur != NULL; i++) {
+        if (i == idx) {
+            return cur->data;
+        }
+        cur = cur->next;
+    }
+
+    fprintf(stderr, "tried to get element at non-existent list position %d\n", idx);
+    exit(EXIT_FAILURE);
+}
+
+size_t length(node_t **head_ref) {
+    node_t *cur = *head_ref;
+    size_t length = 0;
+
+    for (int i = 0; cur != NULL; i++) {
+        length++;
+        cur = cur->next;
+    }
+
+    return length;
+}
+
---
+++ src/list.h
@@ -0,0 +1,21 @@
+#ifndef WASM_INTERPRETER_LIST_H
+#define WASM_INTERPRETER_LIST_H
+
+#include <stdlib.h>
+#include "util.h"
+#include "type.h"
+
+typedef struct node {
+    struct node *next;
+    void *data;
+} node_t;
+
+void insert_first(node_t **head_ref, void *new_data, uint32_t data_size);
+
+void delete_first(node_t **head_ref);
+
+void *get_at(node_t **head_ref, uint32_t idx);
+
+size_t length(node_t **head_ref);
+
+#endif //WASM_INTERPRETER_LIST_H
