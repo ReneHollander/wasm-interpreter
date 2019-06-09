2bf3eeda6f78b9bf3acab7ff9c13eec73644412d
Markus Hinterleitner <m.hinterleitner93@gmail.com>
2019-06-09 09:41:38 +0200

    Adds relational numeric operations.

--- src/numeric.c
+++ src/numeric.c
@@ -11,6 +11,8 @@
 #include "opd_stack.h"
 #include "interpreter.h"
 
+static void eval_rel(instruction_t instr);
+
 static bool is_add(opcode_t opcode);
 
 static bool is_sub(opcode_t opcode);
@@ -20,6 +22,8 @@ static bool is_mul(opcode_t opcode);
 static bool is_div(opcode_t opcode);
 
 static bool is_const(opcode_t opcode);
+
+static bool is_rel(opcode_t opcode);
 
 static i32 add_i32(i32 op1, i32 op2);
 
@@ -84,6 +88,74 @@ static i64 or_i64(i64 op1, i64 op2);
 static i32 and_i32(i32 op1, i32 op2);
 
 static i64 and_i64(i64 op1, i64 op2);
+
+i32 eqz_i32(i32 op);
+
+i32 eq_i32(i32 op1, i32 op2);
+
+i32 ne_i32(i32 op1, i32 op2);
+
+i32 lt_i32_s(i32 op1, i32 op2);
+
+i32 lt_i32_u(u32 op1, u32 op2);
+
+i32 gt_i32_s(i32 op1, i32 op2);
+
+i32 gt_i32_u(u32 op1, u32 op2);
+
+i32 le_i32_s(i32 op1, i32 op2);
+
+i32 le_i32_u(u32 op1, u32 op2);
+
+i32 ge_i32_s(i32 op1, i32 op2);
+
+i32 ge_i32_u(u32 op1, u32 op2);
+
+i32 eqz_i64(i64 op);
+
+i32 eq_i64(i64 op1, i64 op2);
+
+i32 ne_i64(i64 op1, i64 op2);
+
+i32 lt_i64_s(i64 op1, i64 op2);
+
+i32 lt_i64_u(i64 op1, i64 op2);
+
+i32 gt_i64_s(i64 op1, i64 op2);
+
+i32 gt_i64_u(i64 op1, i64 op2);
+
+i32 le_i64_s(i64 op1, i64 op2);
+
+i32 le_i64_u(i64 op1, i64 op2);
+
+i32 ge_i64_s(i64 op1, i64 op2);
+
+i32 ge_i64_u(i64 op1, i64 op2);
+
+i32 eq_f32(f32 op1, f32 op2);
+
+i32 ne_f32(f32 op1, f32 op2);
+
+i32 lt_f32(f32 op1, f32 op2);
+
+i32 gt_f32(f32 op1, f32 op2);
+
+i32 le_f32(f32 op1, f32 op2);
+
+i32 ge_f32(f32 op1, f32 op2);
+
+i32 eq_f64(i64 op1, f64 op2);
+
+i32 ne_f64(f64 op1, f64 op2);
+
+i32 lt_f64(f64 op1, f64 op2);
+
+i32 gt_f64(f64 op1, f64 op2);
+
+i32 le_f64(f64 op1, f64 op2);
+
+i32 ge_f64(f64 op1, f64 op2);
 
 void eval_numeric_instr(instruction_t instr) {
     opcode_t opcode = instr.opcode;
@@ -214,14 +286,159 @@ void eval_numeric_instr(instruction_t instr) {
         i64 op2 = pop_opd_i64();
         i64 op1 = pop_opd_i64();
         push_opd_i64(and_i64(op1, op2));
+    } else if (is_rel(opcode)) {
+        eval_rel(instr);
     } else {
         fprintf(stderr, "numeric instruction with opcode %X currently not supported\n", opcode);
         interpreter_exit();
     }
 }
 
+static void eval_rel(instruction_t instr) {
+    opcode_t opcode = instr.opcode;
+
+    if (opcode == OP_I32_EQZ) {
+        i32 op = pop_opd_i32();
+        push_opd_i32(eqz_i32(op));
+    } else if (opcode == OP_I32_EQ) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(eq_i32(op1, op2));
+    } else if (opcode == OP_I32_NE) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(ne_i32(op1, op2));
+    } else if (opcode == OP_I32_LT_S) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(lt_i32_s(op1, op2));
+    } else if (opcode == OP_I32_LT_U) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(lt_i32_u(op1, op2));
+    } else if (opcode == OP_I32_GE_S) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(ge_i32_s(op1, op2));
+    } else if (opcode == OP_I32_GE_U) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(ge_i32_u(op1, op2));
+    } else if (opcode == OP_I32_GT_S) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(gt_i32_s(op1, op2));
+    } else if (opcode == OP_I32_GT_U) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(gt_i32_u(op1, op2));
+    } else if (opcode == OP_I32_LE_S) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(le_i32_s(op1, op2));
+    } else if (opcode == OP_I32_LE_U) {
+        i32 op2 = pop_opd_i32();
+        i32 op1 = pop_opd_i32();
+        push_opd_i32(le_i32_u(op1, op2));
+    } else if (opcode == OP_I64_EQZ) {
+        i64 op = pop_opd_i64();
+        push_opd_i32(eqz_i64(op));
+    } else if (opcode == OP_I64_EQ) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(eq_i64(op1, op2));
+    } else if (opcode == OP_I64_NE) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(ne_i64(op1, op2));
+    } else if (opcode == OP_I64_LT_S) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(lt_i64_s(op1, op2));
+    } else if (opcode == OP_I64_LT_U) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(lt_i64_u(op1, op2));
+    } else if (opcode == OP_I64_GE_S) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(ge_i64_s(op1, op2));
+    } else if (opcode == OP_I64_GE_U) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(ge_i64_u(op1, op2));
+    } else if (opcode == OP_I64_GT_S) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(gt_i64_s(op1, op2));
+    } else if (opcode == OP_I64_GT_U) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(gt_i64_u(op1, op2));
+    } else if (opcode == OP_I64_LE_S) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(le_i64_s(op1, op2));
+    } else if (opcode == OP_I64_LE_U) {
+        i64 op2 = pop_opd_i64();
+        i64 op1 = pop_opd_i64();
+        push_opd_i32(le_i64_u(op1, op2));
+    } else if (opcode == OP_F32_EQ) {
+        f32 op2 = pop_opd_f32();
+        f32 op1 = pop_opd_f32();
+        push_opd_i32(eq_f32(op1, op2));
+    } else if (opcode == OP_F32_NE) {
+        f32 op2 = pop_opd_f32();
+        f32 op1 = pop_opd_f32();
+        push_opd_i32(ne_f32(op1, op2));
+    } else if (opcode == OP_F32_LT) {
+        f32 op2 = pop_opd_f32();
+        f32 op1 = pop_opd_f32();
+        push_opd_i32(lt_f32(op1, op2));
+    } else if (opcode == OP_F32_GE) {
+        f32 op2 = pop_opd_f32();
+        f32 op1 = pop_opd_f32();
+        push_opd_i32(ge_f32(op1, op2));
+    } else if (opcode == OP_F32_GT) {
+        f32 op2 = pop_opd_f32();
+        f32 op1 = pop_opd_f32();
+        push_opd_i32(gt_f32(op1, op2));
+    } else if (opcode == OP_F32_LE) {
+        f32 op2 = pop_opd_f32();
+        f32 op1 = pop_opd_f32();
+        push_opd_i32(le_f32(op1, op2));
+    } else if (opcode == OP_F64_EQ) {
+        f64 op2 = pop_opd_f64();
+        f64 op1 = pop_opd_f64();
+        push_opd_i32(eq_f64(op1, op2));
+    } else if (opcode == OP_F64_NE) {
+        f64 op2 = pop_opd_f64();
+        f64 op1 = pop_opd_f64();
+        push_opd_i32(ne_f64(op1, op2));
+    } else if (opcode == OP_F64_LT) {
+        f64 op2 = pop_opd_f64();
+        f64 op1 = pop_opd_f64();
+        push_opd_i32(lt_f64(op1, op2));
+    } else if (opcode == OP_F64_GE) {
+        f64 op2 = pop_opd_f64();
+        f64 op1 = pop_opd_f64();
+        push_opd_i32(ge_f64(op1, op2));
+    } else if (opcode == OP_F64_GT) {
+        f64 op2 = pop_opd_f64();
+        f64 op1 = pop_opd_f64();
+        push_opd_i32(gt_f64(op1, op2));
+    } else if (opcode == OP_F64_LE) {
+        f64 op2 = pop_opd_f64();
+        f64 op1 = pop_opd_f64();
+        push_opd_i32(le_f64(op1, op2));
+    } else {
+        interpreter_error("unknown rel opcode\n");
+    }
+}
+
 bool is_numeric_instr(opcode_t opcode) {
-    return is_add(opcode) || is_sub(opcode) || is_mul(opcode) || is_div(opcode) || is_const(opcode);
+    return is_add(opcode) || is_sub(opcode) || is_mul(opcode) ||
+           is_div(opcode) || is_const(opcode) || is_rel(opcode);
 }
 
 static bool is_const(opcode_t opcode) {
@@ -280,6 +497,48 @@ static bool is_div(opcode_t opcode) {
         case OP_I64_DIV_S:
         case OP_I64_DIV_U:
         case OP_F64_DIV:
+            return true;
+        default:
+            return false;
+    }
+}
+
+static bool is_rel(opcode_t opcode) {
+    switch (opcode) {
+        case OP_I32_LT_S:
+        case OP_I32_LT_U:
+        case OP_I32_GE_S:
+        case OP_I32_GE_U:
+        case OP_I32_GT_S:
+        case OP_I32_GT_U:
+        case OP_I32_LE_S:
+        case OP_I32_LE_U:
+        case OP_I32_EQ:
+        case OP_I32_NE:
+        case OP_I64_LT_S:
+        case OP_I64_LT_U:
+        case OP_I64_GE_S:
+        case OP_I64_GE_U:
+        case OP_I64_GT_S:
+        case OP_I64_GT_U:
+        case OP_I64_LE_S:
+        case OP_I64_LE_U:
+        case OP_I64_EQ:
+        case OP_I64_NE:
+        case OP_F32_LT:
+        case OP_F32_GE:
+        case OP_F32_GT:
+        case OP_F32_LE:
+        case OP_F32_EQ:
+        case OP_F32_NE:
+        case OP_F64_LT:
+        case OP_F64_GE:
+        case OP_F64_GT:
+        case OP_F64_LE:
+        case OP_F64_EQ:
+        case OP_F64_NE:
+        case OP_I32_EQZ:
+        case OP_I64_EQZ:
             return true;
         default:
             return false;
@@ -416,6 +675,138 @@ i64 and_i64(i64 op1, i64 op2) {
     return op1 & op2;
 }
 
+i32 eqz_i32(i32 op) {
+    return op == 0 ? 1 : 0;
+}
 
+i32 eq_i32(i32 op1, i32 op2) {
+    return op1 == op2 ? 1 : 0;
+}
 
+i32 ne_i32(i32 op1, i32 op2) {
+    return op1 != op2 ? 1 : 0;
+}
 
+i32 lt_i32_s(i32 op1, i32 op2) {
+    return op1 < op2 ? 1 : 0;
+}
+
+i32 lt_i32_u(u32 op1, u32 op2) {
+    return op1 < op2 ? 1 : 0;
+}
+
+i32 gt_i32_s(i32 op1, i32 op2) {
+    return op1 > op2 ? 1 : 0;
+}
+
+i32 gt_i32_u(u32 op1, u32 op2) {
+    return op1 > op2 ? 1 : 0;
+}
+
+i32 le_i32_s(i32 op1, i32 op2) {
+    return op1 <= op2 ? 1 : 0;
+}
+
+i32 le_i32_u(u32 op1, u32 op2) {
+    return op1 <= op2 ? 1 : 0;
+}
+
+i32 ge_i32_s(i32 op1, i32 op2) {
+    return op1 >= op2 ? 1 : 0;
+}
+
+i32 ge_i32_u(u32 op1, u32 op2) {
+    return op1 >= op2 ? 1 : 0;
+}
+
+i32 eqz_i64(i64 op) {
+    return op == 0 ? 1 : 0;
+}
+
+i32 eq_i64(i64 op1, i64 op2) {
+    return op1 == op2 ? 1 : 0;
+}
+
+i32 ne_i64(i64 op1, i64 op2) {
+    return op1 != op2 ? 1 : 0;
+}
+
+i32 lt_i64_s(i64 op1, i64 op2) {
+    return op1 < op2 ? 1 : 0;
+}
+
+i32 lt_i64_u(i64 op1, i64 op2) {
+    return op1 < op2 ? 1 : 0;
+}
+
+i32 gt_i64_s(i64 op1, i64 op2) {
+    return op1 > op2 ? 1 : 0;
+}
+
+i32 gt_i64_u(i64 op1, i64 op2) {
+    return op1 > op2 ? 1 : 0;
+}
+
+i32 le_i64_s(i64 op1, i64 op2) {
+    return op1 <= op2 ? 1 : 0;
+}
+
+i32 le_i64_u(i64 op1, i64 op2) {
+    return op1 <= op2 ? 1 : 0;
+}
+
+i32 ge_i64_s(i64 op1, i64 op2) {
+    return op1 >= op2 ? 1 : 0;
+}
+
+i32 ge_i64_u(i64 op1, i64 op2) {
+    return op1 >= op2 ? 1 : 0;
+}
+
+i32 eq_f32(f32 op1, f32 op2) {
+    return op1 == op2 ? 1 : 0;
+}
+
+i32 ne_f32(f32 op1, f32 op2) {
+    return op1 != op2 ? 1 : 0;
+}
+
+i32 lt_f32(f32 op1, f32 op2) {
+    return op1 < op2 ? 1 : 0;
+}
+
+i32 gt_f32(f32 op1, f32 op2) {
+    return op1 > op2 ? 1 : 0;
+}
+
+i32 le_f32(f32 op1, f32 op2) {
+    return op1 <= op2 ? 1 : 0;
+}
+
+i32 ge_f32(f32 op1, f32 op2) {
+    return op1 >= op2 ? 1 : 0;
+}
+
+i32 eq_f64(i64 op1, f64 op2) {
+    return op1 == op2 ? 1 : 0;
+}
+
+i32 ne_f64(f64 op1, f64 op2) {
+    return op1 != op2 ? 1 : 0;
+}
+
+i32 lt_f64(f64 op1, f64 op2) {
+    return op1 < op2 ? 1 : 0;
+}
+
+i32 gt_f64(f64 op1, f64 op2) {
+    return op1 > op2 ? 1 : 0;
+}
+
+i32 le_f64(f64 op1, f64 op2) {
+    return op1 <= op2 ? 1 : 0;
+}
+
+i32 ge_f64(f64 op1, f64 op2) {
+    return op1 >= op2 ? 1 : 0;
+}
