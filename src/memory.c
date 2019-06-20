04e499a1049204dff2a531d9f820286263835b42
Rene Hollander <mail@renehollander.at>
2019-06-20 17:15:33 +0200

    Adds additional memory load and store instructions

--- src/memory.c
+++ src/memory.c
@@ -7,6 +7,7 @@
 #include "memory.h"
 #include "interpreter.h"
 #include "opd_stack.h"
+#include "util.h"
 
 static memory_t *memory = NULL;
 
@@ -89,59 +90,76 @@ void eval_memory_instr(instruction_t instr) {
         interpreter_error("no memory found\n");
     }
 
+#define LOAD_INSN(srctype, targettype) \
+    i32 ea = caluclate_address(instr, sizeof(srctype) * 8); \
+    srctype value = ((srctype *) memory->data)[ea]; \
+    CAT(push_opd_, targettype)((targettype) value)
+
+#define FLOAT_LOAD_INSN(type) \
+    i32 ea = caluclate_address(instr, sizeof(type) * 8); \
+    type value = ((type *) memory->data)[ea]; \
+    CAT(push_opd_, type)(value)
+
+#define STORE_INSN(srctype, intermediatetype, targettype) \
+    srctype value = CAT(pop_opd_, srctype)(); \
+    intermediatetype v = (intermediatetype) value; \
+    i32 ea = caluclate_address(instr, sizeof(targettype) * 8); \
+    ((targettype *) memory->data)[ea] = (targettype) (v & BIT_MASK(intermediatetype, sizeof(targettype) * 8))
+
+#define FLOAT_STORE_INSN(type) \
+    type value = CAT(pop_opd_, type)(); \
+    i32 ea = caluclate_address(instr, sizeof(type) * 8); \
+    ((type *) memory->data)[ea] = value
+
     opcode_t opcode = instr.opcode;
     if (opcode == OP_I32_LOAD) {
-        i32 ea = caluclate_address(instr, 32);
-        i32 value = ((i32 *) memory->data)[ea];
-        push_opd_i32(value);
+        LOAD_INSN(i32, i32);
     } else if (opcode == OP_I64_LOAD) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(i64, i64);
     } else if (opcode == OP_F32_LOAD) {
-        interpreter_error("memory opcode not implemented\n");
+        FLOAT_LOAD_INSN(f32);
     } else if (opcode == OP_F64_LOAD) {
-        interpreter_error("memory opcode not implemented\n");
+        FLOAT_LOAD_INSN(f64);
     } else if (opcode == OP_I32_LOAD8_S) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(s8, i32);
     } else if (opcode == OP_I32_LOAD8_U) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(u8, i32);
     } else if (opcode == OP_I32_LOAD16_S) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(s16, i32);
     } else if (opcode == OP_I32_LOAD16_U) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(u16, i32);
     } else if (opcode == OP_I64_LOAD8_S) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(s8, i64);
     } else if (opcode == OP_I64_LOAD8_U) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(u8, i64);
     } else if (opcode == OP_I64_LOAD16_S) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(s16, i64);
     } else if (opcode == OP_I64_LOAD16_U) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(u16, i64);
     } else if (opcode == OP_I64_LOAD32_S) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(s32, i64);
     } else if (opcode == OP_I64_LOAD32_U) {
-        interpreter_error("memory opcode not implemented\n");
+        LOAD_INSN(u32, i64);
     } else if (opcode == OP_I32_STORE) {
-        i32 value = pop_opd_i32();
-        i32 ea = caluclate_address(instr, 32);
-        ((i32 *) memory->data)[ea] = value;
+        STORE_INSN(i32, u32, i32);
     } else if (opcode == OP_I64_STORE) {
-        interpreter_error("memory opcode not implemented\n");
+        STORE_INSN(i64, u64, i64);
     } else if (opcode == OP_F32_STORE) {
-        interpreter_error("memory opcode not implemented\n");
+        FLOAT_STORE_INSN(f32);
     } else if (opcode == OP_F64_STORE) {
-        interpreter_error("memory opcode not implemented\n");
+        FLOAT_STORE_INSN(f64);
     } else if (opcode == OP_I32_STORE8) {
-        interpreter_error("memory opcode not implemented\n");
+        STORE_INSN(i32, u32, u8);
     } else if (opcode == OP_I32_STORE16) {
-        interpreter_error("memory opcode not implemented\n");
+        STORE_INSN(i32, u32, u16);
     } else if (opcode == OP_I64_STORE8) {
-        interpreter_error("memory opcode not implemented\n");
+        STORE_INSN(i64, u64, u8);
     } else if (opcode == OP_I64_STORE16) {
-        interpreter_error("memory opcode not implemented\n");
+        STORE_INSN(i64, u64, u16);
     } else if (opcode == OP_I64_STORE32) {
-        interpreter_error("memory opcode not implemented\n");
+        STORE_INSN(i64, u64, u32);
     } else if (opcode == OP_MEMORY_SIZE) {
-        interpreter_error("memory opcode not implemented\n");
+        push_opd_i32(memory->size);
     } else if (opcode == OP_MEMORY_GROW) {
         interpreter_error("memory opcode not implemented\n");
     } else {
--- src/util.h
+++ src/util.h
@@ -2,8 +2,13 @@
 #define UTIL_H
 
 #include <stdbool.h>
+#include <limits.h>
 
 #define CAT(x, y) CAT_(x, y)
 #define CAT_(x, y) x ## y
+
+#define BIT_MASK(__TYPE__, __ONE_COUNT__) \
+    ((__TYPE__) (-((__ONE_COUNT__) != 0))) \
+    & (((__TYPE__) -1) >> ((sizeof(__TYPE__) * CHAR_BIT) - (__ONE_COUNT__)))
 
 #endif // UTIL_H
